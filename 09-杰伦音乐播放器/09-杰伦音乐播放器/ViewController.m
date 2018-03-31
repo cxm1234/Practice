4f0f0b88c2a357bef142576a67b0d8b6d8e00e48
CXM <245413040@qq.com>
2018-03-31 23:00:17 +0800

    杰伦播放器完工

--- 09-杰伦音乐播放器/09-杰伦音乐播放器/ViewController.m
+++ 09-杰伦音乐播放器/09-杰伦音乐播放器/ViewController.m
@@ -7,11 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *bgImageView;
-
+@property (nonatomic,strong) AVPlayer *player;
 @end
 
 @implementation ViewController
@@ -24,14 +25,41 @@
     toolbar.alpha = 0.98;
     [self.bgImageView addSubview:toolbar];
     NSURL *url = [[NSBundle mainBundle] URLForResource:@"mySong1.mp3" withExtension:nil];
-    
+    AVPlayerItem *playerItem = [[AVPlayerItem alloc] initWithURL:url];
+    self.player = [[AVPlayer alloc] initWithPlayerItem:playerItem];
 }
 
 - (IBAction)playOrPause:(UIButton *)sender {
+    switch (sender.tag) {
+        case 3:
+            [self.player play];
+            break;
+        case 4:
+            [self.player pause];
+            break;
+        default:
+            break;
+    }
+    
 }
 
 
 - (IBAction)changeMusic:(UIButton *)sender {
+     NSString *musicName = nil;
+    switch (sender.tag) {
+        case 1:
+            musicName = @"mySong2.mp3";
+            break;
+        case 2:
+            musicName = @"mySong3.mp3";
+            break;
+        default:
+            break;
+    }
+    NSURL *url = [[NSBundle mainBundle] URLForResource:musicName withExtension:nil];
+    AVPlayerItem *playerItem = [[AVPlayerItem alloc] initWithURL:url];
+    [self.player replaceCurrentItemWithPlayerItem:playerItem];
+    [self.player play];
 }
 
 
