f6e35d43dcd4aeb14164a0be048b587a9e5aa7fe
CXM <245413040@qq.com>
2018-04-05 12:01:55 +0800

    提交
    综合练习

---
+++ 10-综合练习/10-综合练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  10-综合练习
+//
+//  Created by xiaoming on 2018/4/2.
+//  Copyright © 2018年 xiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 10-综合练习/10-综合练习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  10-综合练习
+//
+//  Created by xiaoming on 2018/4/2.
+//  Copyright © 2018年 xiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
