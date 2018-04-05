7fa778d72908ec0aa3d4132922b62f9d54d6d375
CXM <245413040@qq.com>
2018-04-05 12:44:52 +0800

    11-分页控件
    分页控件

---
+++ 11-分页控件/11-分页控件/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11-分页控件
+//
+//  Created by xiaoming on 2018/4/5.
+//  Copyright © 2018年 xiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 11-分页控件/11-分页控件/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  11-分页控件
+//
+//  Created by xiaoming on 2018/4/5.
+//  Copyright © 2018年 xiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
