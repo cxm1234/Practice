489561d532926821d4ddcdb1ed661cf9c6ac3a2d
Andy <245413040@qq.com>
2018-04-20 11:49:42 +0800

    pthread的简单使用
    pthread的简单使用

---
+++ 14-pthread的简单使用/14-pthread的简单使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  14-pthread的简单使用
+//
+//  Created by zkinc on 2018/4/20.
+//  Copyright © 2018年 zkinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
